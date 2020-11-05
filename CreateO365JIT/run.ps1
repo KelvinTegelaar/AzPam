a59ac1fe99c0244b31659539aad36323387c3787
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2020-11-05 23:15:28 +0100

    some changes messing around.

--- CreateO365JIT/run.ps1
+++ CreateO365JIT/run.ps1
@@ -7,10 +7,38 @@ param($Request, $TriggerMetadata)
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 try {
-$Accounts = get-content -Path $ENV:NewO365Account | ConvertFrom-Json
-foreach($Account in $Accounts){
-#Create new account
-}
+  $Accounts = get-content -Path $ENV:NewO365Account | ConvertFrom-Json
+  foreach ($Account in $Accounts) {
+    $Authbody = @{
+      'resource'      = 'https://graph.microsoft.com'
+      'client_id'     = $ENV:ApplicationId
+      'client_secret' = $ENV:ApplicationSecret
+      'grant_type'    = "client_credentials"
+      'scope'         = "openid"
+    }
+    $ClientToken = Invoke-RestMethod -Method post -Uri "https://login.microsoftonline.com/$($Account.tenantid)/oauth2/token" -Body $Authbody -ErrorAction Stop
+    $headers = @{ "Authorization" = "Bearer $($ClientToken.access_token)" }
+  
+    $Body = [PSCustomObject]@{
+      "accountEnabled"    = $true
+      "displayName"       = $Account.Displayname
+      "mailNickname"      = $Account.mail
+      "userPrincipalName" = $account.UPN
+      "passwordProfile"   = @{
+        "forceChangePasswordNextSignIn" = $false
+        "password"                      = $Account.password
+      }
+    } | ConvertTo-Json
+
+    $CreateUserResult = Invoke-RestMethod -Uri 'https://graph.microsoft.com/v1.0/users' -Method POST -Body $Body -Headers $Headers
+    $GroupBody = [PSCustomObject]@{
+      principalId      = $CreateUserResult.UserID
+      roleDefinitionId = $Account.roleDefinitionId
+      directoryScopeId = "/"
+    } 
+  }
+$AddGroupResult = Invoke-RestMethod -Uri 'https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments' -Method POST -Body $GroupBody -Headers $Headers
+
 }
 catch {
   Add-Content -path $ENV:ErrorLog -Value "$($currentUTCtime): Could not create new O365 JIT Account. Error:  $($_.Exception.Message)" -Force
--- RefreshTokens/run.ps1
+++ RefreshTokens/run.ps1
@@ -10,9 +10,7 @@ $TenantID = $ENV:TenantID
 $credential = New-Object System.Management.Automation.PSCredential($ApplicationId, $ApplicationSecret)
 try {
     $token = New-PartnerAccessToken -ApplicationId $ApplicationID -Scopes 'https://api.partnercenter.microsoft.com/user_impersonation' -ServicePrincipal -Credential $credential -Tenant $TenantID -UseAuthorizationCode
-    $Exchangetoken = New-PartnerAccessToken -ApplicationId 'a0c73c16-a7e3-4564-9a95-2bdf47383716' -Scopes 'https://outlook.office365.com/.default' -Tenant $TenantID -UseDeviceAuthentication
     $ENV:RefreshToken = $($token.refreshtoken)
-    $ENV:ExchangeRefreshToken = $($ExchangeToken.Refreshtoken)
 }
 catch {
     Add-Content -path $ENV:ErrorLog -Value "$($currentUTCtime): Could not refresh tokens. Error:  $($_.Exception.Message)" -Force
