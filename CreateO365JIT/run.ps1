4b305ed2b22070edb5b881b7b22a3d8d34b4531f
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2020-11-07 17:50:30 +0100

    First working model able to create accounts and assign a role.

--- CreateO365JIT/run.ps1
+++ CreateO365JIT/run.ps1
@@ -1,11 +1,18 @@
 using namespace System.Net
-
-# Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
+function New-output ($ReturnedBody) {
+  Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+      StatusCode  = [HttpStatusCode]::OK
+      Body        = $ReturnedBody
+      ContentType = 'text/json'
+    }) 
+}
+# Input bindings are passed in via param block.
 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 $Account = [System.Web.HttpUtility]::ParseQueryString($Request.Body)
+
 try {
   $Authbody = @{
     'resource'      = 'https://graph.microsoft.com'
@@ -16,10 +23,17 @@ try {
   }
   $ClientToken = Invoke-RestMethod -Method post -Uri "https://login.microsoftonline.com/$($Account['tenantid'])/oauth2/token" -Body $Authbody -ErrorAction Stop
   $headers = @{ "Authorization" = "Bearer $($ClientToken.access_token)" }
-  
-  $Body = [PSCustomObject]@{
+}
+catch {
+  Add-Content -path $ENV:ErrorLog -Value "$($currentUTCtime): Could not create new O365 JIT Account. Error:  $($_.Exception.Message)" -Force
+  New-output -ReturnedBody "Graph Login Failure: $($_.Exception.Message)"
+  break
+}
+
+try {
+  $CreateBody = [PSCustomObject]@{
     "accountEnabled"    = $true
-    "displayName"       = $Account['Displayname']
+    "displayName"       = $Account['displayname']
     "mailNickname"      = $Account['mail']
     "userPrincipalName" = $account['UPN']
     "passwordProfile"   = @{
@@ -27,29 +41,30 @@ try {
       "password"                      = $Account['password']
     }
   } | ConvertTo-Json
-
-  $CreateUserResult = Invoke-RestMethod -Uri 'https://graph.microsoft.com/v1.0/users' -Method POST -Body $Body -Headers $Headers
-  $GroupBody = [PSCustomObject]@{
-    principalId      = $CreateUserResult.UserID
-    roleDefinitionId = $Account['roleDefinitionId']
-    directoryScopeId = "/"
-  } 
-  
-  $AddGroupResult = Invoke-RestMethod -Uri 'https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments' -Method POST -Body $GroupBody -Headers $Headers
-
-  #New-AccountTrace -AccountID $($CreateUserResult.id) -ExpectedGroups $AddGroupResult.id
-  $ReturnedBody = "success"
+#New-Output -ReturnedBody $Body
+  $CreateUserResult = Invoke-RestMethod -Uri 'https://graph.microsoft.com/v1.0/users' -ContentType 'Application/json' -Method POST -Body $CreateBody -Headers $Headers
 }
 catch {
   Add-Content -path $ENV:ErrorLog -Value "$($currentUTCtime): Could not create new O365 JIT Account. Error:  $($_.Exception.Message)" -Force
-  $ReturnedBody = 'failure'
+  New-output -ReturnedBody "User Add Failure: $($_.Exception.Message)"
+  break
 }
 
-# Interact with query parameters or the body of the request.
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode  = [HttpStatusCode]::OK
-    Body        = $ReturnedBody
-    ContentType = 'text/html'
-  }) 
+try{
+$GroupBody = [PSCustomObject]@{
+  principalId      = $CreateUserResult.ID
+  roleDefinitionId = $Account['roleDefinitionId']
+  directoryScopeId = "/"
+} | ConvertTo-Json
   
+$AddGroupResult = Invoke-RestMethod -Uri 'https://graph.microsoft.com/beta/roleManagement/directory/roleAssignments' -Method POST -ContentType 'Application/json' -Body $GroupBody -Headers $Headers
+
+#New-AccountTrace -AccountID $($CreateUserResult.id) -ExpectedGroups $AddGroupResult.id
+New-output -ReturnedBody "Success"
+}
+catch {
+  break
+  Add-Content -path $ENV:ErrorLog -Value "$($currentUTCtime): Could not create new O365 JIT Account. Error:  $($_.Exception.Message)" -Force
+  New-output -ReturnedBody "Group Add Failure: $($_.Exception.Message)"
+}
+
